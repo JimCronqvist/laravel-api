6d16ca94bc3ed50f97bd53145bd99b6cae94e584
Jim Cronqvist <jim.cronqvist@gmail.com>
2020-11-10 23:05:14 +0100

    Media on the fly wip

--- src/Http/Controllers/MediaOnTheFly.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Cronqvist\Api\Http\Controllers;
-
-trait MediaOnTheFly
-{
-
-}
---
+++ src/Services/MediaLibrary/MediaOnTheFly.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Cronqvist\Api\Services\MediaLibrary;
+
+use Cronqvist\Api\Http\Middleware\JsonMiddleware;
+use Illuminate\Http\File as FileObject;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Filesystem;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\ImageFactory;
+
+class MediaOnTheFly
+{
+    protected $media;
+    protected $deleteAfterSend = true;
+    protected $manipulations;
+
+    public function __construct(Media $media)
+    {
+        $this->media = $media;
+        $this->manipulations = $this->parseManipulationsFromRequest();
+    }
+
+    protected function rules()
+    {
+        return [
+            'width' => 'int|min:1|required_if:crop,1',
+            'height' => 'int|min:1|required_if:crop,1',
+            'optimize' => 'bool|in:1',
+            'format' => 'in:webp',
+            'quality' => 'int|between:1,100',
+            'crop' => 'bool|in:1',
+        ];
+    }
+
+    protected function getValidatedOptionsFromRequest()
+    {
+        $query = request()->query();
+        try {
+            $validated = Validator::make($query, $this->rules())->validate();
+        } catch (ValidationException $e) {
+            // Ensure JSON
+            (new JsonMiddleware())->handle(request(), fn() => 1);
+            throw $e;
+        }
+        return $validated;
+    }
+
+    protected function parseManipulationsFromRequest()
+    {
+        $options = $this->getValidatedOptionsFromRequest();
+
+        $manipulations = new Manipulations();
+        if(isset($options['width'], $options['height'])) {
+            $fit = empty($options['crop']) ? Manipulations::FIT_MAX : Manipulations::FIT_CROP;
+            $manipulations->fit($fit, $options['width'], $options['height']);
+            unset($options['width'], $options['height'], $options['crop']);
+        }
+        foreach($options as $option => $value) {
+            if(array_key_exists($option, $options)) {
+                if(in_array($option, ['optimize'])) {
+                    $manipulations->{$option}();
+                } else {
+                    $manipulations->{$option}($value);
+                }
+            }
+        }
+        return $manipulations;
+    }
+
+    protected function getFilenameSuffix()
+    {
+        $options = current($this->manipulations->toArray());
+        if(!$options) return null;
+        ksort($options);
+
+        $string = '';
+        foreach($options as $key => $value) {
+            $string .= substr($key, 0, 1);
+            $string .= strpos($value, '[') === false ? trim($value) : '';
+            $string .= '_';
+        }
+        return '__' . trim($string, '_');
+    }
+
+    protected function getCacheDir()
+    {
+        $cacheDir = config('media-library.cache_directory_path') ?? storage_path('media-library/cache');
+        return '/' . trim($cacheDir, '/') . '/';
+    }
+
+    protected function getOutputFilename($suffix, $extension)
+    {
+        return pathinfo($this->media->file_name, PATHINFO_FILENAME) . $suffix . '.' . $extension;
+    }
+
+    protected function isDiskDriverLocal()
+    {
+        return $this->media->getDiskDriverName() == 'local';
+    }
+
+    protected function getExtension()
+    {
+        return $this->manipulations->hasManipulation('format')
+            ? $this->manipulations->getFirstManipulationArgument('format')
+            : strtolower($this->media->getExtensionAttribute());
+    }
+
+    protected function ensureCacheDirExists()
+    {
+        $cacheDir = $this->getCacheDir();
+        if(!File::isDirectory($cacheDir)) {
+            File::makeDirectory($cacheDir);
+        }
+    }
+
+    protected function perform()
+    {
+        $cacheDir = $this->getCacheDir();
+        $localOriginalFile = $this->media->getPath() . $this->media->file_name;
+
+        if(!$this->isDiskDriverLocal()) {
+            $localOriginalFile = $cacheDir . $this->media->uuid . '.' . strtolower($this->media->getExtensionAttribute());
+            if(!is_readable($localOriginalFile)) {
+                $this->ensureCacheDirExists();
+                app(Filesystem::class)->copyFromMediaLibrary($this->media, $localOriginalFile);
+            }
+        }
+
+        if($this->manipulations->isEmpty()) {
+            if($this->isDiskDriverLocal()) {
+                // Never delete the original file if that one is used directly
+                $this->deleteAfterSend = false;
+            }
+            $file = new FileObject($localOriginalFile);
+            return [$file, $file->getFilename()];
+        }
+
+        $suffix = $this->getFilenameSuffix();
+        $extension = $this->getExtension();
+        $transformFile = $cacheDir . $this->media->uuid . $suffix . '.' . $extension;
+
+        if(!is_readable($transformFile)) {
+            if($this->isDiskDriverLocal()) {
+                $this->ensureCacheDirExists();
+            }
+
+            File::copy($localOriginalFile, $transformFile);
+
+            ImageFactory::load($transformFile)
+                ->manipulate($this->manipulations)
+                ->save();
+        }
+
+        return [
+            new FileObject($transformFile),
+            $this->getOutputFilename($suffix, $extension)
+        ];
+    }
+
+    public function cache()
+    {
+        $this->deleteAfterSend = false;
+        return $this;
+    }
+
+    public function output()
+    {
+        [$file, $filename] = $this->perform();
+        return response()
+            ->download($file, $filename, [], 'inline')
+            ->deleteFileAfterSend($this->deleteAfterSend);
+    }
+
+    public function download()
+    {
+        [$file, $filename] = $this->perform();
+        return response()
+            ->download($file, $filename)
+            ->deleteFileAfterSend($this->deleteAfterSend);
+    }
+}
