a25762d765813c3a46e20ab984250d1cc807c574
Jim Cronqvist <jim.cronqvist@gmail.com>
2020-11-17 16:00:44 +0100

    Media on the fly wip

--- src/Console/Commands/ApiMediaCacheClean.php
+++ src/Console/Commands/ApiMediaCacheClean.php
@@ -4,7 +4,9 @@ namespace Cronqvist\Api\Console\Commands;
 
 use Cronqvist\Api\Services\MediaLibrary\MediaOnTheFly;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Finder\Finder;
 
 class ApiMediaCacheClean extends Command
 {
@@ -37,8 +39,41 @@ class ApiMediaCacheClean extends Command
      */
     public function handle()
     {
-        $path = MediaOnTheFly::getCacheDir();
-        $files = Storage::allFiles($path);
-        dd($files);
+        $directory = MediaOnTheFly::getCacheDir();
+        $files = iterator_to_array(
+            Finder::create()
+                ->files()
+                ->ignoreDotFiles(false)
+                ->in($directory)
+                ->sortByModifiedTime()
+                ->reverseSorting()
+            , false
+        );
+        $allowed = 1024 * 1024 * 1024 * (int) $this->argument('GB');
+        $size = 0;
+        $deleted = 0;
+        foreach($files as $file) {
+            /** @var $file \Symfony\Component\Finder\SplFileInfo */
+            $size += $file->getSize();
+
+            if($size > $allowed) {
+                File::delete($file->getPathname());
+                $deleted++;
+            }
+        }
+
+        $humanReadableBytes = function($bytes) {
+            if($bytes == 0) return '0.00 B';
+            $s = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+            $e = floor(log($bytes, 1024));
+            return round($bytes/pow(1024, $e), 2) . ' ' . $s[$e];
+        };
+
+        return printf(
+            "Total size: %s\nSize allowed: %s\nFiles deleted: %s\n",
+            $humanReadableBytes($size),
+            $humanReadableBytes($allowed),
+            $deleted
+        );
     }
 }
