7993ef89762d6ebbaa54551bb2aa292cc7884835
Jim Cronqvist <jim.cronqvist@gmail.com>
2020-12-07 21:37:26 +0100

    Fixes for Laravel 8 where having a Models folder is the new default

--- src/Console/Commands/ApiPolicyMakeCommand.php
+++ src/Console/Commands/ApiPolicyMakeCommand.php
@@ -51,7 +51,7 @@ class ApiPolicyMakeCommand extends BasePolicyMakeCommand
     protected function replaceModel($stub, $model)
     {
         $stub = parent::replaceModel($stub, $model);
-        $model = app()->getNamespace() . str_replace('/', '\\', $model);
+        $model = app()->getNamespace() . 'Models\\' . str_replace('/', '\\', $model);
         $table = (new $model)->getTable();
         return str_replace('DummyTable', $table, $stub);
     }
