1f3528f9308410559f2deacba22a8158864062bb
Jim Cronqvist <jim.cronqvist@gmail.com>
2025-11-12 15:20:35 +0100

    Added a custom cast when a boolean can be nullable, to force a boolean anyway.

---
+++ src/Models/Casts/BooleanNullableCast.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class BooleanNullableCast implements CastsAttributes
+{
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return (bool) $value;
+    }
+
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return $value ? 1 : 0;
+    }
+}
