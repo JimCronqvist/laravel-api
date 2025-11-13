ba15c57bb6699906f1b246129d8b0c93c157ecb8
Jim Cronqvist <jim.cronqvist@gmail.com>
2025-11-13 14:25:47 +0100

    Added initial support and structure for nested routes for relations

--- src/Policies/Policy.php
+++ src/Policies/Policy.php
@@ -147,4 +147,195 @@ abstract class Policy
 
         return $this->isAllowed($user, $relation.'.detach');
     }
+
+    /**
+     * Determine whether the user can view multiple related models.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasManyIndex(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view a specific related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasManyShow(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        if($this->isGuestsAllowed(__FUNCTION__)) return true;
+
+        return $this->isAllowed($user, $relation.'.view') && $this->isAllowedRaw($user, $childModel->getTable().'.view');
+    }
+
+    /**
+     * Determine whether the user can create a related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasManyStore(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update a specific related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasManyUpdate(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete a specific related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasManyDestroy(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasOneShow(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can upsert (create or update) the related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasOneUpsert(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationHasOneDestroy(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationBelongsToShow(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view multiple related models.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationBelongsToManyIndex(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view a specific related model.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @param \Illuminate\Database\Eloquent\Model $childModel
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationBelongsToManyShow(?User $user, Model $parentModel, string $relation, Model $childModel)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can sync related models.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationBelongsToManySync(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view or manipulate pivot attributes of the relation.
+     *
+     * @param \App\User|null $user
+     * @param \Illuminate\Database\Eloquent\Model $parentModel
+     * @param string $relation
+     * @return bool
+     * @throws \App\Exceptions\ApiException
+     */
+    public function relationBelongsToManyPivot(?User $user, Model $parentModel, string $relation)
+    {
+        return false;
+    }
 }
