6a772e39b1252b6c586350228638172f823bad7f
Jim Cronqvist <jim.cronqvist@gmail.com>
2026-04-13 21:07:04 +0200

    Add middleware to normalize client IP addresses (ipv4 mapped ipv6 addresses)

--- src/Http/Middleware/NormalizeClientIps.php
+++ src/Http/Middleware/NormalizeClientIps.php
@@ -7,20 +7,26 @@ use Illuminate\Http\Request;
 
 class NormalizeClientIps
 {
+    public static array $replaceHeaders = [
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR',
+    ];
+
     public function handle(Request $request, Closure $next)
     {
-        // Normalize IPv4-mapped IPv6 addresses in X-Forwarded-For
-        if(str_contains($request->server('HTTP_X_FORWARDED_FOR', ''), '::ffff:')) {
-            $normalizedIps = collect(explode(',', $request->server('HTTP_X_FORWARDED_FOR')))
-                ->map(fn($ip) => preg_replace('/^::ffff:/', '', trim($ip)))
-                ->implode(', ');
+        // Normalize IPv4-mapped IPv6 addresses in headers like X-Forwarded-For and REMOTE_ADDR
+        foreach(static::$replaceHeaders as $header) {
+            $value = $request->server($header, '');
+            if(str_contains($value, '::ffff:')) {
+                $normalizedIps = collect(explode(',', $value))
+                    ->map(fn($ip) => str_starts_with($ip = trim($ip), '::ffff:') ? substr($ip, 7) : $ip)
+                    ->implode(', ');
 
-            $request->server->set('HTTP_X_FORWARDED_FOR', $normalizedIps);
-        }
-
-        // Normalize IPv4-mapped IPv6 addresses in REMOTE_ADDR
-        if(str_contains($request->server('REMOTE_ADDR', ''), '::ffff:')) {
-            $request->server->set('REMOTE_ADDR', preg_replace('/^::ffff:/', '', $request->server('REMOTE_ADDR')));
+                $request->server->set($header, $normalizedIps);
+                if(str_starts_with($header, 'HTTP_')) {
+                    $request->headers->set(str_replace('HTTP_', '', $header), $normalizedIps);
+                }
+            }
         }
 
         return $next($request);
