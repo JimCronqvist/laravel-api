d4f11790a5156df4bdbb8c98de7167a775420247
Jim Cronqvist <jim.cronqvist@gmail.com>
2026-04-13 14:47:49 +0200

    Add initial support for multi-tenant sso auth via a wrapper around socialite, and oidc.

--- src/Auth/routes/sso.php
+++ src/Auth/routes/sso.php
@@ -18,8 +18,10 @@ Route::namespace('Cronqvist\Api\Auth\SSO\Http\Controllers')->middleware('api')->
     // Public endpoints
     //
 
-    Route::get('/auth/sso/{provider}/redirect', [SsoController::class, 'redirect'])->name('sso.redirect')->withoutMiddleware('api');
-    Route::get('/auth/sso/{provider}/callback', [SsoController::class, 'callback'])->name('sso.callback')->withoutMiddleware('api');
+    Route::get('/auth/sso/{provider}/redirect', [SsoController::class, 'redirect'])->name('sso.redirect')
+        ->withoutMiddleware('api.json');
+    Route::get('/auth/sso/{provider}/callback', [SsoController::class, 'callback'])->name('sso.callback')
+        ->withoutMiddleware('api.json');
     Route::get('/auth/sso/providers/{emailOrDomain?}', [SsoController::class, 'providers'])->name('sso.providers');
     Route::post('/auth/sso/exchange', [SsoController::class, 'exchange'])->name('sso.exchange');
 });
